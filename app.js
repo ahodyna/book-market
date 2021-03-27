6f978d3f90ce655d009c6dd1c3e527faa9824b52
alina <hodyna.alina@gmail.com>
2021-03-27 22:08:54 +0200

    first implementation of book market

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'ejs'))
+
+app.get('/', (req, res) => {
+  res.render('main')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
