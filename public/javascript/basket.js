167822314f848db777c85c3beeb070e7180a6419
alina <hodyna.alina@gmail.com>
2021-05-02 15:37:15 +0300

    changed approach to add items to cart

--- public/javascript/basket.js
+++ public/javascript/basket.js
@@ -1,23 +1,41 @@
-
 const dataKey = 'bookIds';
 
-function addToCart(dataBookElemId) {
-   const idsArray = JSON.parse(localStorage.getItem(dataKey))
+function cookieToMap(cookies) {
+   if (cookies.length == 0) {
+      return new Map();
+   }
 
-   let bookIds = new Set(idsArray)
-   console.log('bookIds', bookIds)
+   let cookiesMap = new Map();
 
-   bookIds.add(dataBookElemId);
-   localStorage.setItem(dataKey, JSON.stringify(Array.from(bookIds)))
+   let arrCookies = cookies.split(';')
+   for (let i = 0; i < arrCookies.length; i++) {
+      let elem = arrCookies[i];
+      elemArr = elem.split('=')
+      cookiesMap.set(elemArr[0].trim(), elemArr[1].trim())
+   }
+
+   return cookiesMap
+} 
+
+function addToCart(productId) {
+   const cookiesMap = cookieToMap(document.cookie)
+   let booksIdsStr = cookiesMap.get(dataKey)
+   
+   let booksIds = booksIdsStr == undefined? [] : JSON.parse(booksIdsStr);
+   booksIds.push(productId)
+
+   booksIdsStr = JSON.stringify(booksIds)
+   document.cookie = `${dataKey}=${booksIdsStr}`;
 
    refreshBasketCountNumber()
 }
 
 function refreshBasketCountNumber() {
-   const bookIds = JSON.parse(localStorage.getItem(dataKey))
-   let count = bookIds == null? 0: bookIds.length
+
+   const cookiesMap = cookieToMap(document.cookie)
+   let booksIdsStr = cookiesMap.get(dataKey)
+   let count = booksIdsStr == undefined? 0: JSON.parse(booksIdsStr).length
    document.getElementById('basketCountNumber').innerText = count
 }
-
 
 refreshBasketCountNumber();
